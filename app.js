0d57a40443eba5bbd00235a280aaaa3d625a624c
tluds15 <147005611+tluds15@users.noreply.github.com>
2023-10-11 16:38:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function show_home(){
     tag("canvas").innerHTML=` 
     <div class="center-screen">
     
-    <p><img height="${window.innerHeight * .6}" src="images/motel.png"></p>
+    <p><img height="${window.innerHeight * .6}" src="images/motel_logo.jpeg"></p>
     <div style="text-align:center"></div>
     
     
