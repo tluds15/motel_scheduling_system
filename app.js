bc424eee095898c780080c5f8186632ff00efc27
tluds15 <147005611+tluds15@users.noreply.github.com>
2023-12-06 16:58:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -126,7 +126,8 @@ async function show_rooms(){
             html.push(`<td>${record.fields.Room}</td>`)
             html.push(`<td>${record.fields.Beds}</td>`)
             html.push(`<td>${record.fields.Location}</td>`)
-            html.push(`<td><a href="${record.fields.url}" target="_blank">Click to see room</a></td>`)
+            //html.push(`<td><a href="${record.fields.url}" target="_blank">Click to see room</a></td>`)
+            html.push(`<td><link src="${record.fields.url}" target="_blank"></td>`)
             html.push('</tr>')
         }
  
